060cc340c06151738c77c58262965c9496c31e20
FabriceMbarushimana <f.mbarushim@alustudent.com>
2025-07-06 22:11:10 +0200

    Python - Classes and Objects

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,4 +5,5 @@ class Square:
     """Class that defines a square."""
 
     def __init__(self, size):
+         """Initializion of  the square with a private size attribute."""
         self.__size = size
