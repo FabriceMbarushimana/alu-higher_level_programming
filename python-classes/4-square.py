859930e1ce9244eeea1f078f87448da518f4aa20
FabriceMbarushimana <f.mbarushim@alustudent.com>
2025-07-07 21:44:57 +0200

    Area of square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,14 +3,9 @@
 
 
 class Square:
-    """
-    Define class that represents a square class
-    """
-
+    """ Define class that represents a square class """
     def __init__(self, size=0):
-        """
-        Initialization a new instance of class.
-        """
+        """Initialization a new instance of class. """
         self.size = size
 
     @property
@@ -26,18 +21,14 @@ class Square:
         """
         Define method to set the size of the square.
         """
-        
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
         if value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
         """
         Calculates and returns the area of the square.
-
         """
         return self.__size ** 2
