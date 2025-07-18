e7045edb513a006542f119a6732d6efa0fc3caea
FabriceMbarushimana <f.mbarushim@alustudent.com>
2025-07-18 11:20:02 -0700

    Basic SQL commands

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
---Create table and add record in it
+-- ctreates table and insert records in it 
+
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
